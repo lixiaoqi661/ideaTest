28477c217ff6f0a4caf837c4a985b48779d3829e
lixoaqi321 <lixiaoqi321@163.com>
2020-06-08 16:26:56 +0800

    idea

---
+++ src/hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Classname hello
+ * @Description TODO
+ * @Date 2020/6/8 16:25
+ * @Created by 李晓琪
+ **/
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
